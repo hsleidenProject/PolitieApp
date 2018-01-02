d7883ce72581251c0f5357588dbb1a712532298a
Eddie <s1103183@student.hsleiden.nl>
2018-01-02 16:58:37 +0100

    Test

--- app/src/main/java/com/example/yanick/politieapp/MainActivity.java
+++ app/src/main/java/com/example/yanick/politieapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Testing 123
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
